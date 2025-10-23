a7b054a2ba4cdab3d84d31d47873f1b2b84f3902
WINDOWS-PC\Windows <ntd119@gmai.com>
2025-10-23 16:39:41 +0700

    add

--- src/main/java/apinexo/common/configuration/AppConfig.java
+++ src/main/java/apinexo/common/configuration/AppConfig.java
@@ -74,12 +74,19 @@ public class AppConfig {
         return factory;
     }
 
+//    @Bean
+//    SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+//        http.cors(Customizer.withDefaults()).csrf(csrf -> csrf.disable())
+//                .authorizeHttpRequests(
+//                        auth -> auth.requestMatchers("/api/public/**").permitAll().anyRequest().authenticated())
+//                .oauth2ResourceServer(oauth2 -> oauth2.jwt(Customizer.withDefaults()));
+//        return http.build();
+//    }
     @Bean
     SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        http.cors(Customizer.withDefaults()).csrf(csrf -> csrf.disable())
-                .authorizeHttpRequests(
-                        auth -> auth.requestMatchers("/api/public/**").permitAll().anyRequest().authenticated())
-                .oauth2ResourceServer(oauth2 -> oauth2.jwt(Customizer.withDefaults()));
+        http
+            .csrf(csrf -> csrf.disable())
+            .authorizeHttpRequests(auth -> auth.anyRequest().permitAll());
         return http.build();
     }
 }
--- src/main/java/apinexo/core/modules/plans/facade/impl/PlansFacadeImpl.java
+++ src/main/java/apinexo/core/modules/plans/facade/impl/PlansFacadeImpl.java
@@ -19,7 +19,47 @@ public class PlansFacadeImpl extends AbstractService implements PlansFacade {
     @Override
     public ResponseEntity<Object> plans(Jwt jwt, String id) {
         try {
-            return ResponseEntity.ok(id);
+            String json = "[\r\n" + "    {\r\n" + "        \"id\": \"68b948cbddae44eacad9317c\",\r\n"
+                    + "        \"nickname\": \"Basic\",\r\n" + "        \"key\": \"basic\",\r\n"
+                    + "        \"up_to\": 200,\r\n" + "        \"period\": \"month\",\r\n"
+                    + "        \"currency\": null,\r\n" + "        \"active\": true,\r\n"
+                    + "        \"price\": null,\r\n" + "        \"is_free\": true,\r\n"
+                    + "        \"overage_prices\": [],\r\n" + "        \"metadata\": {\r\n"
+                    + "            \"key\": \"basic\",\r\n" + "            \"rate_limit\": 1000,\r\n"
+                    + "            \"rate_limit_period\": \"hour\",\r\n" + "            \"is_soft_limit\": false,\r\n"
+                    + "            \"private\": false,\r\n" + "            \"api_id\": \"jsearch\"\r\n"
+                    + "        }\r\n" + "    },\r\n" + "    {\r\n"
+                    + "        \"id\": \"price_1S3YCmRoRAtc95u1thyjr81b\",\r\n" + "        \"nickname\": \"Pro\",\r\n"
+                    + "        \"key\": \"pro\",\r\n" + "        \"up_to\": 10000,\r\n"
+                    + "        \"period\": \"month\",\r\n" + "        \"currency\": \"usd\",\r\n"
+                    + "        \"active\": true,\r\n" + "        \"price\": 2500,\r\n"
+                    + "        \"is_free\": false,\r\n" + "        \"overage_prices\": [\r\n"
+                    + "            \"0.3\"\r\n" + "        ],\r\n" + "        \"metadata\": {\r\n"
+                    + "            \"api_id\": \"jsearch\",\r\n" + "            \"is_soft_limit\": \"true\",\r\n"
+                    + "            \"key\": \"pro\",\r\n" + "            \"private\": \"false\",\r\n"
+                    + "            \"rate_limit\": \"5\",\r\n" + "            \"rate_limit_period\": \"second\"\r\n"
+                    + "        }\r\n" + "    },\r\n" + "    {\r\n"
+                    + "        \"id\": \"price_1S3YCmRoRAtc95u19GnkLda0\",\r\n" + "        \"nickname\": \"Ultra\",\r\n"
+                    + "        \"key\": \"ultra\",\r\n" + "        \"up_to\": 50000,\r\n"
+                    + "        \"period\": \"month\",\r\n" + "        \"currency\": \"usd\",\r\n"
+                    + "        \"active\": true,\r\n" + "        \"price\": 7500,\r\n"
+                    + "        \"is_free\": false,\r\n" + "        \"overage_prices\": [\r\n"
+                    + "            \"0.2\"\r\n" + "        ],\r\n" + "        \"metadata\": {\r\n"
+                    + "            \"api_id\": \"jsearch\",\r\n" + "            \"is_soft_limit\": \"true\",\r\n"
+                    + "            \"key\": \"ultra\",\r\n" + "            \"private\": \"false\",\r\n"
+                    + "            \"rate_limit\": \"10\",\r\n" + "            \"rate_limit_period\": \"second\"\r\n"
+                    + "        }\r\n" + "    },\r\n" + "    {\r\n"
+                    + "        \"id\": \"price_1S3YCnRoRAtc95u1LW4pDadp\",\r\n" + "        \"nickname\": \"Mega\",\r\n"
+                    + "        \"key\": \"mega\",\r\n" + "        \"up_to\": 200000,\r\n"
+                    + "        \"period\": \"month\",\r\n" + "        \"currency\": \"usd\",\r\n"
+                    + "        \"active\": true,\r\n" + "        \"price\": 15000,\r\n"
+                    + "        \"is_free\": false,\r\n" + "        \"overage_prices\": [\r\n"
+                    + "            \"0.1\"\r\n" + "        ],\r\n" + "        \"metadata\": {\r\n"
+                    + "            \"api_id\": \"jsearch\",\r\n" + "            \"is_soft_limit\": \"true\",\r\n"
+                    + "            \"key\": \"mega\",\r\n" + "            \"private\": \"false\",\r\n"
+                    + "            \"rate_limit\": \"20\",\r\n" + "            \"rate_limit_period\": \"second\"\r\n"
+                    + "        }\r\n" + "    }\r\n" + "]";
+            return ResponseEntity.ok(utils.convertStrToJson(json));
         } catch (HttpClientErrorException ex) {
             return ResponseEntity.status(ex.getStatusCode()).body(utils.err(ex.getMessage()));
         } catch (Exception ex) {
