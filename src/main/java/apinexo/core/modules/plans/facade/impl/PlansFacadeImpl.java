05e7e59423dbf89d53251e8989b4bec15d8e0784
WINDOWS-PC\Windows <ntd119@gmai.com>
2025-10-25 19:59:16 +0700

    add

--- src/main/java/apinexo/core/modules/plans/facade/impl/PlansFacadeImpl.java
+++ src/main/java/apinexo/core/modules/plans/facade/impl/PlansFacadeImpl.java
@@ -1,16 +1,19 @@
 package apinexo.core.modules.plans.facade.impl;
 
+import java.util.Optional;
+
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.oauth2.jwt.Jwt;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.HttpClientErrorException;
 
-import com.fasterxml.jackson.databind.JsonNode;
-
+import apinexo.client.exception.ApiException;
 import apinexo.common.dtos.AbstractService;
 import apinexo.common.utils.ApinexoUtils;
+import apinexo.core.modules.plans.dto.ApiPlansResponse;
+import apinexo.core.modules.plans.entity.ApiPlansEntity;
 import apinexo.core.modules.plans.facade.PlansFacade;
-import apinexo.core.modules.stripe.service.StripeService;
+import apinexo.core.modules.plans.service.ApiPlansService;
 import lombok.RequiredArgsConstructor;
 
 @Component
@@ -19,13 +22,22 @@ public class PlansFacadeImpl extends AbstractService implements PlansFacade {
 
     private final ApinexoUtils utils;
 
-    private final StripeService stripeService;
+    private final ApiPlansService apiPlansService;
 
     @Override
     public ResponseEntity<Object> plans(Jwt jwt, String id) {
         try {
-            JsonNode response = stripeService.createPriceHardLimit(null);
-            return ResponseEntity.ok(response);
+            Optional<ApiPlansEntity> optional = apiPlansService.findByid(id.toLowerCase());
+            if (optional.isPresent()) {
+                ApiPlansEntity entity = optional.get();
+                ApiPlansResponse response = ApiPlansResponse.builder().id(entity.getId())
+                        .basic(utils.convertStrToJson(entity.getBasic())).pro(utils.convertStrToJson(entity.getPro()))
+                        .ultra(utils.convertStrToJson(entity.getUltra())).mega(utils.convertStrToJson(entity.getMega()))
+                        .build();
+                return ResponseEntity.ok(response);
+            } else {
+                return ResponseEntity.badRequest().body(new ApiException("The plans does not exist"));
+            }
         } catch (HttpClientErrorException ex) {
             return ResponseEntity.status(ex.getStatusCode()).body(utils.err(ex.getMessage()));
         } catch (Exception ex) {
