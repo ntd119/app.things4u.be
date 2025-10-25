7e5d133a61c2b268eb58407ab2ec99096118427b
WINDOWS-PC\Windows <ntd119@gmai.com>
2025-10-25 11:01:51 +0700

    add

--- src/main/java/apinexo/core/modules/plans/facade/impl/PlansFacadeImpl.java
+++ src/main/java/apinexo/core/modules/plans/facade/impl/PlansFacadeImpl.java
@@ -33,25 +33,41 @@ public class PlansFacadeImpl extends AbstractService implements PlansFacade {
             HttpHeaders headers = utils.buildHeader();
             headers.setContentType(MediaType.APPLICATION_FORM_URLENCODED);
             headers.setBasicAuth(stripeSecret, "");
-            MultiValueMap<String, String> body = new LinkedMultiValueMap<>();
-
+//            MultiValueMap<String, String> body = new LinkedMultiValueMap<>();
+//            body.add("currency", "usd");
+//            body.add("recurring[interval]", "month");
+//            body.add("recurring[usage_type]", "metered");
+//            body.add("billing_scheme", "tiered");
+//            body.add("tiers_mode", "graduated");
+//            body.add("product_data[name]", "JSearch");
+//            body.add("nickname", "Pro");
+//            body.add("tiers[0][up_to]", "10000");
+//            body.add("tiers[0][unit_amount]", "0");
+//            body.add("tiers[1][up_to]", "inf");
+//            body.add("tiers[1][unit_amount_decimal]", "0.003");
+//            body.add("metadata[api_id]", "jsearch");
+//            body.add("metadata[key]", "pro");
+//            body.add("metadata[is_soft_limit]", "true");
+//            body.add("metadata[rate_limit]", "5");
+//            body.add("metadata[rate_limit_period]", "second");
+            String body =
+                    "currency=usd" +
+                    "&recurring[interval]=month" +
+                    "&recurring[usage_type]=metered" +
+                    "&billing_scheme=tiered" +
+                    "&tiers_mode=graduated" +
+                    "&product_data[name]=JSearch" +
+                    "&nickname=Pro" +
+                    "&tiers[0][up_to]=10000" +
+                    "&tiers[0][unit_amount]=0" +
+                    "&tiers[1][up_to]=inf" +
+                    "&tiers[1][unit_amount_decimal]=0.003" +
+                    "&metadata[api_id]=jsearch" +
+                    "&metadata[key]=pro" +
+                    "&metadata[is_soft_limit]=true" +
+                    "&metadata[rate_limit]=5" +
+                    "&metadata[rate_limit_period]=second";
             String url = "https://api.stripe.com/v1/prices";
-            body.add("currency", "usd");
-            body.add("recurring[interval]", "month");
-            body.add("recurring[usage_type]", "metered");
-            body.add("billing_scheme", "tiered");
-            body.add("tiers_mode", "graduated");
-            body.add("product_data[name]", "JSearch");
-            body.add("nickname", "Pro");
-            body.add("tiers[0][up_to]", "10000");
-            body.add("tiers[0][unit_amount]", "0");
-            body.add("tiers[1][up_to]", "inf");
-            body.add("tiers[1][unit_amount_decimal]", "0.003");
-            body.add("metadata[api_id]", "jsearch");
-            body.add("metadata[key]", "pro");
-            body.add("metadata[is_soft_limit]", "true");
-            body.add("metadata[rate_limit]", "5");
-            body.add("metadata[rate_limit_period]", "second");
             JsonNode response = executePostRequest(JsonNode.class, url, body, headers);
 
             return ResponseEntity.ok(response);
--- src/main/java/apinexo/core/modules/subscription/facade/impl/SubscriptionFacadeImpl.java
+++ src/main/java/apinexo/core/modules/subscription/facade/impl/SubscriptionFacadeImpl.java
@@ -3,46 +3,48 @@ package apinexo.core.modules.subscription.facade.impl;
 import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.oauth2.jwt.Jwt;
 import org.springframework.stereotype.Component;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
 
-import com.stripe.Stripe;
-import com.stripe.model.checkout.Session;
-import com.stripe.param.checkout.SessionCreateParams;
+import com.fasterxml.jackson.databind.JsonNode;
 
+import apinexo.common.dtos.AbstractService;
 import apinexo.common.utils.ApinexoUtils;
 import apinexo.core.modules.subscription.facade.SubscriptionFacade;
 import lombok.RequiredArgsConstructor;
 
 @Component
 @RequiredArgsConstructor
-public class SubscriptionFacadeImpl implements SubscriptionFacade {
-
-    // private final OpenmeterService openmeterService;
+public class SubscriptionFacadeImpl extends AbstractService implements SubscriptionFacade {
 
     private final ApinexoUtils utils;
 
     @Value("${stripe.secret.key}")
-    private String stripeSecretKey;
+    private String stripeSecret;
 
     @Override
     public ResponseEntity<Object> changeSubscription(Jwt jwt, Map<String, Object> body) {
         try {
             String userEmail = "marketing202001@gmail.com";
-            String userId = utils.uuidRandom();
-            Stripe.apiKey = stripeSecretKey;
-            String priceId = "price_1SLgjuR5bl4Qw3RDJTJDzfDB";
-            SessionCreateParams params = SessionCreateParams.builder().setMode(SessionCreateParams.Mode.SUBSCRIPTION)
-                    .setCustomerEmail(userEmail)
-                    .setSuccessUrl("https://your-frontend.com/success?session_id={CHECKOUT_SESSION_ID}")
-                    .setCancelUrl("https://your-frontend.com/cancel")
-                    .addLineItem(SessionCreateParams.LineItem.builder().setPrice(priceId).setQuantity(1L).build())
-                    .putMetadata("userId", userId).putMetadata("planKey", "Pro").build();
-
-            Session session = Session.create(params);
-
-            return ResponseEntity.ok(Map.of("checkout_url", session.getUrl(), "session_id", session.getId()));
+            HttpHeaders headers = utils.buildHeader();
+            headers.setContentType(MediaType.APPLICATION_FORM_URLENCODED);
+            headers.setBasicAuth(stripeSecret, "");
+            MultiValueMap<String, String> bodyClient = new LinkedMultiValueMap<>();
+            bodyClient.add("mode", "subscription"); 
+            bodyClient.add("success_url", "https://your-domain.com/success?session_id={CHECKOUT_SESSION_ID}");
+            bodyClient.add("cancel_url", "https://your-domain.com/cancel");
+            bodyClient.add("customer_email", userEmail);
+            bodyClient.add("line_items[0][price]", "price_1SLySER5bl4Qw3RDCSX3DqlQ");
+            bodyClient.add("line_items[0][quantity]", "1");
+            bodyClient.add("line_items[1][price]", "price_1SLyGvR5bl4Qw3RDY6UDhkeG");
+            String url = "https://api.stripe.com/v1/checkout/sessions";
+            JsonNode response = executePostRequest(JsonNode.class, url, bodyClient, headers);
+            return ResponseEntity.ok(response);
         } catch (Exception ex) {
             return ResponseEntity.badRequest().body(ex.getMessage());
         }
