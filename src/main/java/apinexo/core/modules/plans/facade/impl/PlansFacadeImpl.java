9a37c1643b0cf5d46714539c1d94eb35b6ccb7c7
WINDOWS-PC\Windows <ntd119@gmai.com>
2025-10-25 10:18:30 +0700

    add

--- src/main/java/apinexo/core/modules/plans/facade/impl/PlansFacadeImpl.java
+++ src/main/java/apinexo/core/modules/plans/facade/impl/PlansFacadeImpl.java
@@ -1,9 +1,16 @@
 package apinexo.core.modules.plans.facade.impl;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.oauth2.jwt.Jwt;
 import org.springframework.stereotype.Component;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
 import org.springframework.web.client.HttpClientErrorException;
+
+import com.fasterxml.jackson.databind.JsonNode;
 
 import apinexo.common.dtos.AbstractService;
 import apinexo.common.utils.ApinexoUtils;
@@ -16,50 +23,38 @@ public class PlansFacadeImpl extends AbstractService implements PlansFacade {
 
     private final ApinexoUtils utils;
 
+    @Value("${stripe.secret.key}")
+    private String stripeSecret;
+
     @Override
     public ResponseEntity<Object> plans(Jwt jwt, String id) {
         try {
-            String json = "[\r\n" + "    {\r\n" + "        \"id\": \"68b948cbddae44eacad9317c\",\r\n"
-                    + "        \"nickname\": \"Basic\",\r\n" + "        \"key\": \"basic\",\r\n"
-                    + "        \"up_to\": 200,\r\n" + "        \"period\": \"month\",\r\n"
-                    + "        \"currency\": null,\r\n" + "        \"active\": true,\r\n"
-                    + "        \"price\": null,\r\n" + "        \"is_free\": true,\r\n"
-                    + "        \"overage_prices\": [],\r\n" + "        \"metadata\": {\r\n"
-                    + "            \"key\": \"basic\",\r\n" + "            \"rate_limit\": 1000,\r\n"
-                    + "            \"rate_limit_period\": \"hour\",\r\n" + "            \"is_soft_limit\": false,\r\n"
-                    + "            \"private\": false,\r\n" + "            \"api_id\": \"jsearch\"\r\n"
-                    + "        }\r\n" + "    },\r\n" + "    {\r\n"
-                    + "        \"id\": \"price_1S3YCmRoRAtc95u1thyjr81b\",\r\n" + "        \"nickname\": \"Pro\",\r\n"
-                    + "        \"key\": \"pro\",\r\n" + "        \"up_to\": 10000,\r\n"
-                    + "        \"period\": \"month\",\r\n" + "        \"currency\": \"usd\",\r\n"
-                    + "        \"active\": true,\r\n" + "        \"price\": 2500,\r\n"
-                    + "        \"is_free\": false,\r\n" + "        \"overage_prices\": [\r\n"
-                    + "            \"0.3\"\r\n" + "        ],\r\n" + "        \"metadata\": {\r\n"
-                    + "            \"api_id\": \"jsearch\",\r\n" + "            \"is_soft_limit\": \"true\",\r\n"
-                    + "            \"key\": \"pro\",\r\n" + "            \"private\": \"false\",\r\n"
-                    + "            \"rate_limit\": \"5\",\r\n" + "            \"rate_limit_period\": \"second\"\r\n"
-                    + "        }\r\n" + "    },\r\n" + "    {\r\n"
-                    + "        \"id\": \"price_1S3YCmRoRAtc95u19GnkLda0\",\r\n" + "        \"nickname\": \"Ultra\",\r\n"
-                    + "        \"key\": \"ultra\",\r\n" + "        \"up_to\": 50000,\r\n"
-                    + "        \"period\": \"month\",\r\n" + "        \"currency\": \"usd\",\r\n"
-                    + "        \"active\": true,\r\n" + "        \"price\": 7500,\r\n"
-                    + "        \"is_free\": false,\r\n" + "        \"overage_prices\": [\r\n"
-                    + "            \"0.2\"\r\n" + "        ],\r\n" + "        \"metadata\": {\r\n"
-                    + "            \"api_id\": \"jsearch\",\r\n" + "            \"is_soft_limit\": \"true\",\r\n"
-                    + "            \"key\": \"ultra\",\r\n" + "            \"private\": \"false\",\r\n"
-                    + "            \"rate_limit\": \"10\",\r\n" + "            \"rate_limit_period\": \"second\"\r\n"
-                    + "        }\r\n" + "    },\r\n" + "    {\r\n"
-                    + "        \"id\": \"price_1S3YCnRoRAtc95u1LW4pDadp\",\r\n" + "        \"nickname\": \"Mega\",\r\n"
-                    + "        \"key\": \"mega\",\r\n" + "        \"up_to\": 200000,\r\n"
-                    + "        \"period\": \"month\",\r\n" + "        \"currency\": \"usd\",\r\n"
-                    + "        \"active\": true,\r\n" + "        \"price\": 15000,\r\n"
-                    + "        \"is_free\": false,\r\n" + "        \"overage_prices\": [\r\n"
-                    + "            \"0.1\"\r\n" + "        ],\r\n" + "        \"metadata\": {\r\n"
-                    + "            \"api_id\": \"jsearch\",\r\n" + "            \"is_soft_limit\": \"true\",\r\n"
-                    + "            \"key\": \"mega\",\r\n" + "            \"private\": \"false\",\r\n"
-                    + "            \"rate_limit\": \"20\",\r\n" + "            \"rate_limit_period\": \"second\"\r\n"
-                    + "        }\r\n" + "    }\r\n" + "]";
-            return ResponseEntity.ok(utils.convertStrToJson(json));
+
+            HttpHeaders headers = utils.buildHeader();
+            headers.setContentType(MediaType.APPLICATION_FORM_URLENCODED);
+            headers.setBasicAuth(stripeSecret, "");
+            MultiValueMap<String, String> body = new LinkedMultiValueMap<>();
+
+            String url = "https://api.stripe.com/v1/prices";
+            body.add("currency", "usd");
+            body.add("recurring[interval]", "month");
+            body.add("recurring[usage_type]", "metered");
+            body.add("billing_scheme", "tiered");
+            body.add("tiers_mode", "graduated");
+            body.add("product_data[name]", "JSearch");
+            body.add("nickname", "Pro");
+            body.add("tiers[0][up_to]", "10000");
+            body.add("tiers[0][unit_amount]", "0");
+            body.add("tiers[1][up_to]", "inf");
+            body.add("tiers[1][unit_amount_decimal]", "0.003");
+            body.add("metadata[api_id]", "jsearch");
+            body.add("metadata[key]", "pro");
+            body.add("metadata[is_soft_limit]", "true");
+            body.add("metadata[rate_limit]", "5");
+            body.add("metadata[rate_limit_period]", "second");
+            JsonNode response = executePostRequest(JsonNode.class, url, body, headers);
+
+            return ResponseEntity.ok(response);
         } catch (HttpClientErrorException ex) {
             return ResponseEntity.status(ex.getStatusCode()).body(utils.err(ex.getMessage()));
         } catch (Exception ex) {
