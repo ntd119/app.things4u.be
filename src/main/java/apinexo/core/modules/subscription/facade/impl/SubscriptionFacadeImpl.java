7766374b13a287feb15efe2ab2e69a72547dd8be
WINDOWS-PC\Windows <ntd119@gmai.com>
2025-10-27 15:20:46 +0700

    add

--- src/main/java/apinexo/core/modules/subscription/facade/impl/SubscriptionFacadeImpl.java
+++ src/main/java/apinexo/core/modules/subscription/facade/impl/SubscriptionFacadeImpl.java
@@ -53,27 +53,38 @@ public class SubscriptionFacadeImpl extends AbstractService implements Subscript
                 return utils.badRequest("The plans does not exist");
             }
 
-            String priceId = plansEntities.stream().filter(plan -> plan.getKey().equalsIgnoreCase(body.getPlanKey()))
-                    .map(PlansEntity::getId).findFirst().orElse(null);
+            Optional<PlansEntity> plansOptional = plansEntities.stream()
+                    .filter(plan -> plan.getKey().equalsIgnoreCase(body.getPlanKey())).findFirst();
+            if (!plansOptional.isPresent()) {
+                return utils.badRequest("The plans does not exist");
+            }
+            PlansEntity plansEntity = plansOptional.get();
+            if (plansEntity.getIsFree()) {
+                return ResponseEntity.ok("OK");
+            } else {
+                String priceId = plansEntities.stream()
+                        .filter(plan -> plan.getKey().equalsIgnoreCase(body.getPlanKey())).map(PlansEntity::getId)
+                        .findFirst().orElse(null);
 
-            String userEmail = user.get().getEmail();
-            HttpHeaders headers = utils.buildHeader();
-            headers.setContentType(MediaType.APPLICATION_FORM_URLENCODED);
-            headers.setBasicAuth(stripeSecret, "");
-            MultiValueMap<String, String> bodyClient = new LinkedMultiValueMap<>();
-            bodyClient.add("mode", "subscription");
-            bodyClient.add("success_url", body.getUrl());
-            bodyClient.add("cancel_url", body.getUrl());
-            bodyClient.add("customer_email", userEmail);
-            bodyClient.add("line_items[0][price]", priceId);
-            bodyClient.add("line_items[0][quantity]", "1");
-            bodyClient.add("payment_method_types[0]", "card");
-            bodyClient.add("payment_method_types[1]", "link");
-            String url = "https://api.stripe.com/v1/checkout/sessions";
-            JsonNode response = executePostRequest(JsonNode.class, url, bodyClient, headers);
-            SubscriptionChangeSubscriptionResponse subscriptionResponse = SubscriptionChangeSubscriptionResponse
-                    .builder().url(utils.jsonNodeAt(response, "/url", String.class)).build();
-            return ResponseEntity.ok(subscriptionResponse);
+                String userEmail = user.get().getEmail();
+                HttpHeaders headers = utils.buildHeader();
+                headers.setContentType(MediaType.APPLICATION_FORM_URLENCODED);
+                headers.setBasicAuth(stripeSecret, "");
+                MultiValueMap<String, String> bodyClient = new LinkedMultiValueMap<>();
+                bodyClient.add("mode", "subscription");
+                bodyClient.add("success_url", body.getUrl());
+                bodyClient.add("cancel_url", body.getUrl());
+                bodyClient.add("customer_email", userEmail);
+                bodyClient.add("line_items[0][price]", priceId);
+                bodyClient.add("line_items[0][quantity]", "1");
+                bodyClient.add("payment_method_types[0]", "card");
+                bodyClient.add("payment_method_types[1]", "link");
+                String url = "https://api.stripe.com/v1/checkout/sessions";
+                JsonNode response = executePostRequest(JsonNode.class, url, bodyClient, headers);
+                SubscriptionChangeSubscriptionResponse subscriptionResponse = SubscriptionChangeSubscriptionResponse
+                        .builder().url(utils.jsonNodeAt(response, "/url", String.class)).build();
+                return ResponseEntity.ok(subscriptionResponse);
+            }
         } catch (Exception ex) {
             return ResponseEntity.badRequest().body(ex.getMessage());
         }
