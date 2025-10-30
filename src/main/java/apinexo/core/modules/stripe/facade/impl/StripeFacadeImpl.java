3d8ba52f59c9aa2327e21d8a3028e69d109b5ea4
WINDOWS-PC\Windows <ntd119@gmai.com>
2025-10-30 20:00:43 +0700

    add

--- src/main/java/apinexo/core/modules/stripe/facade/impl/StripeFacadeImpl.java
+++ src/main/java/apinexo/core/modules/stripe/facade/impl/StripeFacadeImpl.java
@@ -1,9 +1,15 @@
 package apinexo.core.modules.stripe.facade.impl;
 
+import java.util.Scanner;
+
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.HttpClientErrorException;
+
+import com.stripe.model.Event;
+import com.stripe.model.PaymentIntent;
+import com.stripe.net.Webhook;
 
 import apinexo.common.dtos.AbstractService;
 import apinexo.common.utils.ApinexoUtils;
@@ -17,12 +23,22 @@ public class StripeFacadeImpl extends AbstractService implements StripeFacade {
 
     private final ApinexoUtils utils;
 
-    @Value("${stripe.secret.key}")
-    private String stripeSecret;
+    @Value("${stripe.secret.endpoint}")
+    private String stripeSecretEndpoint;
 
     @Override
     public ResponseEntity<Object> webhook(HttpServletRequest request) {
         try {
+            String payload = new Scanner(request.getInputStream(), "UTF-8").useDelimiter("\\A").next();
+            String sigHeader = request.getHeader("Stripe-Signature");
+            Event event = Webhook.constructEvent(payload, sigHeader, stripeSecretEndpoint);
+            switch (event.getType()) {
+            case "payment_intent.succeeded":
+                PaymentIntent intent = (PaymentIntent) event.getDataObjectDeserializer().getObject().orElse(null);
+                break;
+            case "payment_intent.payment_failed":
+                break;
+            }
             return ResponseEntity.ok("OK");
         } catch (HttpClientErrorException ex) {
             return ResponseEntity.status(ex.getStatusCode()).body(utils.err(ex.getMessage()));
