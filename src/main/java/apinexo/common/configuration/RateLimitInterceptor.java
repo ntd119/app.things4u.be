20597248e311358d31192631bdd7a352ec9bb954
WINDOWS-PC\Windows <ntd119@gmai.com>
2025-11-21 16:11:49 +0700

    add

---
+++ src/main/java/apinexo/common/configuration/RateLimitInterceptor.java
@@ -0,0 +1,17 @@
+package apinexo.common.configuration;
+
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.HandlerInterceptor;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+@Component
+public class RateLimitInterceptor implements HandlerInterceptor {
+
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler)
+            throws Exception {
+        return HandlerInterceptor.super.preHandle(request, response, handler);
+    }
+}
