9b0c993406526a1dc0ab94e473a79d5b5ecc8612
WINDOWS-PC\Windows <ntd119@gmai.com>
2025-11-24 16:10:25 +0700

    add

--- src/main/java/apinexo/common/configuration/AppConfig.java
+++ src/main/java/apinexo/common/configuration/AppConfig.java
@@ -85,7 +85,9 @@ public class AppConfig {
     @Bean
     @Order(1)
     SecurityFilterChain devFilterChain(HttpSecurity http) throws Exception {
-        http.securityMatcher("/dev/**").authorizeHttpRequests(auth -> auth.anyRequest().authenticated())
+        http.securityMatcher("/dev/**")
+                .authorizeHttpRequests(
+                        auth -> auth.requestMatchers("/dev/stripe/webhook").permitAll().anyRequest().authenticated())
                 .oauth2ResourceServer(oauth2 -> oauth2.jwt(Customizer.withDefaults())).csrf(csrf -> csrf.disable())
                 .cors(Customizer.withDefaults());
         return http.build();
