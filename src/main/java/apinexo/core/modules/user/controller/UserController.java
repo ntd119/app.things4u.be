1251c7a43e7bdc9775ad4f2fe1b4bf10a3dfbac0
WINDOWS-PC\Windows <ntd119@gmai.com>
2025-11-27 15:12:25 +0700

    add

--- src/main/java/apinexo/core/modules/user/controller/UserController.java
+++ src/main/java/apinexo/core/modules/user/controller/UserController.java
@@ -4,6 +4,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.oauth2.jwt.Jwt;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -29,7 +30,7 @@ public class UserController {
         return facade.resetApiKey(jwt);
     }
 
-    @GetMapping("/update-profile")
+    @PostMapping("/update-profile")
     public ResponseEntity<Object> updateProfile(@AuthenticationPrincipal Jwt jwt, @RequestBody UserUpdateProfileRequest request) {
         return facade.updateProfile(jwt, request);
     }
